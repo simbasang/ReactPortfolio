3c11465440a0b200824869ce552890fb8979811a
simbasang <sebastian.englund@live.se>
2024-07-30 23:24:31 +0200

    added font awesome and base for tailwind

---
+++ fontawesome.ts
@@ -0,0 +1,7 @@
+import { config, library } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+// Prevent Font Awesome from adding its CSS automatically
+config.autoAddCss = false;
+library.add(faLinkedin);
--- src/components/MediaBar.tsx
+++ src/components/MediaBar.tsx
@@ -1,16 +1,49 @@
-import facebookIcon from "../assets/social-media/facebook.svg";
-import githubIcon from "../assets/social-media/github.svg";
-import linkedinIcon from "../assets/social-media/linkedin.svg";
 import "../Styles/MediaBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
 function MediaBar() {
   return (
     <>
       <div className="media-bar-container">
         <div className="media-line" />
-        <img className="social-media-icon" src={linkedinIcon} alt="" />
-        <img className="social-media-icon" src={githubIcon} alt="" />
-        <img className="social-media-icon" src={facebookIcon} alt="" />
+        <a
+          href="https://www.linkedin.com/in/sebastian-englund-b73955194/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="social-media-icon"
+            icon={faLinkedin}
+            style={{ color: "#ffffff" }}
+          />
+        </a>
+        <a
+          href="https://github.com/simbasang"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="social-media-icon"
+            icon={faGithub}
+            style={{ color: "#ffffff" }}
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/sebastian.l.englund/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="social-media-icon"
+            icon={faFacebook}
+            style={{ color: "#ffffff" }}
+          />
+        </a>
       </div>
     </>
   );
