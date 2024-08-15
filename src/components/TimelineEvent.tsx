b20ce8eea63fbe95fba0c292265b302e20d9e0d4
simbasang <sebastian.englund@live.se>
2024-08-15 23:04:10 +0200

    styling for timeline

--- src/components/TimelineEvent.tsx
+++ src/components/TimelineEvent.tsx
@@ -10,21 +10,44 @@ interface Props {
 function TimelineEvent(props: Props) {
   return (
     <>
-      <div className="timeline-top-container">
-        <div className="timeline-year-container">
-          <div className="year">{props.year}</div>
-          <div className="year-pointer"></div>
-        </div>
-        <div className="dot-line-container ">
-          <div className="dot-container">
-            <div className="dot"></div>
+      <div className="origin">
+        <div className="timeline-top-container">
+          <div className="timeline-year-container">
+            <div className="year">{props.year}</div>
+            <div className="year-pointer"></div>
           </div>
-          <div className="verticle-line"></div>
+          <div className="dot-line-container ">
+            <div className="dot-container">
+              <div className="dot"></div>
+            </div>
+            <div className="verticle-line"></div>
+          </div>
+          <div className="timeline-text">
+            <h2>{props.title}</h2>
+            <h4>{props.underTitle}</h4>
+            <p>{props.text}</p>
+          </div>
         </div>
-        <div className="timeline-text">
-          <h2>{props.title}</h2>
-          <h4>{props.underTitle}</h4>
-          <p>{props.text}</p>
+      </div>
+      <div className="mobile">
+        <div className="timeline-top-container">
+          <div className="timeline-year-container">
+            <div className="year">{props.year}</div>
+            <div className="year-pointer"></div>
+          </div>
+          <div className="dot-line-container ">
+            <div className="dot-container">
+              <div className="dot"></div>
+            </div>
+          </div>
+        </div>
+        <div className="mobile-line-text">
+          <div className="verticle-line"></div>
+          <div className="timeline-text">
+            <h2>{props.title}</h2>
+            <h4>{props.underTitle}</h4>
+            <p>{props.text}</p>
+          </div>
         </div>
       </div>
     </>
